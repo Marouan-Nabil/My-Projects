e4e0f881c9015cab97aca38548231a495ee5df4e
One <One@DESKTOP-NBQILM8>
2017-11-05 12:13:32 +0100

    Add commentaire

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entrez deux nombre :");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            double c = a + b;
+            Console.WriteLine("som = {0}",c);
+            Console.ReadKey();
+        }
+    }
+}
